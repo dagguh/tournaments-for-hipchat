cc3556996df1ad284a58bc490002e47592a5f7a5
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2016-04-14 20:22:55 +0200

    Test the full `/tournament show` case from `README`

--- src/test/java/pl/dagguh/tournaments/hipchat/TournamentHipchatResourceIT.kt
+++ src/test/java/pl/dagguh/tournaments/hipchat/TournamentHipchatResourceIT.kt
@@ -1,5 +1,6 @@
 package pl.dagguh.tournaments.hipchat
 
+import org.hamcrest.CoreMatchers.equalTo
 import org.junit.Assert.assertThat
 import org.junit.Test
 import pl.dagguh.tournaments.IntegrationEnvironment
@@ -15,14 +16,11 @@ class TournamentHipchatResourceIT {
     val hipchat: WebTarget = IntegrationEnvironment().getRoot().path("hipchat")
 
     @Test
-    fun shouldStartSwiss() {
-        val actualResponse = command(buildCommand(
-                "/tournament start swiss:2:PT65M:4 \"Netrunner Draft #5\" Alice Bob Carol Dave Eve Frank Grace"
-        ).build())
+    fun shouldStartTournament() {
+        val startResponse = command(buildCommand("/tournament start swiss:2:PT65M:4 Title Alice Bob").build())
 
-        assertThat(actualResponse.getString("message"), matchesPattern("Tournament T[\\d]* started\\.".toRegex()))
+        assertThat(startResponse.getString("message"), matchesPattern(START_RESPONSE_PATTERN))
     }
-
 
     private fun buildCommand(message: String): JsonObjectBuilder {
         return createObjectBuilder()
@@ -43,7 +41,35 @@ class TournamentHipchatResourceIT {
                 .readEntity(JsonObject::class.java)
     }
 
+    @Test
+    fun shouldStartAndShowTheNewTournament() {
+        val startResponseMessage = command(buildCommand(
+                "/tournament start swiss:2:PT65M:4 \"Netrunner Draft #5\" @Alice @Bob Carol @Dave @Eve Frank Grace"
+        ).build()).getString("message")
+        val tournamentId = START_RESPONSE_PATTERN
+                .matchEntire(startResponseMessage)
+                ?.groups
+                ?.get(START_RESPONSE_PATTERN_ID_GROUP)
+                ?.value
+                ?.toLong()
+        val showResponseMessage = command(buildCommand(
+                "/tournament show T%d".format(tournamentId)
+        ).build()).getString("message")
+
+        assertThat(showResponseMessage, equalTo("""
+            Tournament `T1` “Netrunner Draft #5” has started on 2016-05-16 17:35.
+
+            It uses Swiss pairings. Number of rounds: 2. Time for each round: 65 minutes. Byes are worth 4 points.
+
+            Participants: Alice Alpha, Bob Beta, Carol, Dave Delta, Eve Epsilon, Frank, Grace
+
+            It is the current tournament.
+        """))
+    }
+
     companion object {
         private val DUMMY_ROOM_ID = 40L
+        private val START_RESPONSE_PATTERN = "Tournament T([\\d]*) started\\.".toRegex()
+        private val START_RESPONSE_PATTERN_ID_GROUP = 1
     }
 }
